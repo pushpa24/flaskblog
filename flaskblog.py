de8ec520bd559a35b2c9d3b63d6a8e34b053caad
pushpa24 <pushpa.iyer17@siesgst.ac.in>
2021-05-24 22:10:16 +0530

    First Commit

---
+++ flaskblog.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts=[
+    {
+        'author':'Pushpa Iyer',
+        'title':'Blog post1',
+        'content':'post1',
+        'date_posted':'April 21, 2018'
+    },
+    {
+        'author':'Aditi Chandrashekar',
+        'title':'Blog post2',
+        'content':'post2',
+        'date_posted':'April 22, 2018'
+    }
+]
+
+@app.route("/")
+def hello():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
