04ccbaa08dffd11d63d8d0532d413213d0bec168
pushpa24 <pushpa.iyer17@siesgst.ac.in>
2021-05-29 00:47:18 +0530

    Added login and regiter partially

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template,url_for
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='620393bcb6a7e844abad8ea480635232'
 
 posts=[
     {
@@ -23,6 +26,16 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title='register',form=form)
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='login',form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
